91dd13104c7715dbe9e7defdb92642d40420b504
Вова Барибін <oknalogia@outlook.com>
2022-05-25 23:34:11 +0300

    fixed styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,5 @@ export { default as Fleet } from './Fleet';
 export { default as ContactUs } from './ContactUs';
 export { default as OffersPage } from './OffersPage';
 export { default as NotFound } from './NotFound';
+export { default as SignIn } from './SignIn';
 export { default as PageUnderConstruction } from './PageUnderConstruction';
