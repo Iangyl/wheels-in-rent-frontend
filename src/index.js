36aabfc46c18cb12c2e3b239dd76f2f4c92dfeca
Вова Барибін <oknalogia@outlook.com>
2022-06-10 21:25:11 +0300

    added react persitor

--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ import './styles/selectSearch.scss';
 import './styles/pagination.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react'
+import {Provider} from 'react-redux';
+import {store, persistor} from './modules/storage';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
